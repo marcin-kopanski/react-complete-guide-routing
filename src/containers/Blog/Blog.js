4532b8a551499bf6bd496c4536a4b576f1a228d1
Marcin Kopanski <mk@pwrs.pl>
2018-04-18 14:57:50 +0200

    guards

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,11 +1,14 @@
 import React, {Component} from 'react';
-import {Route,NavLink,Switch,Redirect} from 'react-router-dom';
+import {Route, NavLink, Switch, Redirect} from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
 
 class Blog extends Component {
+	state = {
+		auth: false
+	}
 
 	render() {
 
@@ -15,11 +18,11 @@ class Blog extends Component {
 					<nav>
 						<ul>
 							<li><NavLink exact to="/posts"
-								activeClassName="my-active"
-								activeStyle={{
-									color: '#fa923f',
-									textDecoration: 'underline'
-								}}>Home</NavLink></li>
+							             activeClassName="my-active"
+							             activeStyle={{
+								             color: '#fa923f',
+								             textDecoration: 'underline'
+							             }}>Home</NavLink></li>
 							<li><NavLink to={{
 								pathname: '/new-post',
 								// pathname: this.props.match.url + '/new-post',
@@ -31,9 +34,9 @@ class Blog extends Component {
 				</header>
 
 				<Switch>
-					<Route path="/new-post" component={NewPost} />
-					<Route path="/posts" component={Posts} />
-					<Redirect from="/" to="/posts" />
+					{this.state.auth ? <Route path="/new-post" component={NewPost}/> : null}
+					<Route path="/posts" component={Posts}/>
+					<Redirect from="/" to="/posts"/>
 				</Switch>
 			</div>
 		);
